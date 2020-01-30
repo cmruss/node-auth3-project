55a502fa9ebbd76c4bc1e4b8c2e9ac541695e182
cmruss <cmrussell08@gmail.com>
2020-01-29 20:07:02 -0500

    cleaned up code

--- users/users-router.js
+++ users/users-router.js
@@ -5,22 +5,12 @@ const restricted = require('../auth/auth-middleware');
 
 //** ENDPOINT /api/users */
 router.get('/', restricted, (req, res) => {
-    console.log(req.user)
+    // console.log(req.user)
     USERS.findBy(req.user)
         .then(users => {
             res.json(users);
         })
         .catch(err => res.status(500).json(err));
 });
-
-function byDepartment(dept) {
-    return function(req, res, next) {
-        if(req.user && req.user.department && req.user.department.toLowercase() === dept) {
-            next();
-        } else {
-            res.status(403).json({ errorMessage: 'Could not retreive users in that department.'})
-        };
-    };
-}
 
 module.exports = router;
